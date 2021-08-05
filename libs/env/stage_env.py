8f4424b8b3df5182eb27fedc8aa2e3ea03c0db13
ducta1 <pX_5Lv5AQV>
2021-08-05 16:24:33 +0700

    add new env files

---
+++ libs/env/stage_env.py
@@ -0,0 +1,122 @@
+################### Global variables ###################
+
+G_language                                  = 'en-US'
+G_root_url                                  = 'https://stage.qc.coccoc.com/'
+G_headless                                  = '!true'
+
+################## Admin account info ##################
+
+G_admin_user_name                           = 'stage_admin@qc.coccoc.com'
+G_admin_user_password                       = 'abc123'
+
+################## Admin account info ##################
+
+G_guru_support_user_name                    = 'stage_gurusupport@qc.coccoc.com'
+G_guru_support_user_password                = 'abc123'
+
+################### Normal user info ###################
+
+G_normal_user_name                          = 'ducta1@coccoc.com'
+G_normal_user_password                      = 'abc123'
+G_normal_user_id                            = '442142'
+G_normal_user_wallet_id                     = '310892'
+
+################### Support user info ###################
+
+G_support_user_name                         = 'stage_support@invite.com'
+G_support_user_password                     = 'abc123'
+G_support_user_id                           = '451446'
+
+#################   Common info     ####################
+
+G_bid_strategy_type                         = ['Manual', 'Automated with daily limit']
+G_daily_limit_type                          = ['Unlimited', 'By budget', 'By impressions']
+G_max_impression_type                       = ['Unlimited', 'Limited']
+G_delivery_type                             = ['Standard', 'Accelerated']
+G_stats_tracking                            = ['On', 'Off']
+
+################# Negative Keyword info ################
+
+Negative_keyword_max_keyword                = 500
+Negative_keyword_max_length                 = 512
+
+################# Browser Skin info ####################
+
+Browser_skin_location                       = 'New Tab'
+Browser_skin_position                       = 'Skin'
+Browser_skin_campaign_type                  = 'Browser Skin'
+Browser_skin_max_name_length                = 130
+Browser_skin_min_bid                        = '66,000'
+Browser_skin_max_bid                        = '15,000,000'
+Browser_skin_min_daily_limit_budget         = '714,000'
+Browser_skin_max_daily_limit_budget         = '700,000,000'
+Browser_skin_min_daily_limit_impression     = '1,000'
+Browser_skin_max_daily_limit_impression     = '10,000,000'
+Browser_skin_min_impressions                = '1'
+Browser_skin_max_impressions                = '1,000,000,000'
+
+################# Search Ads info ####################
+
+Search_ads_location                         = 'Search Result'
+Search_ads_campaign_type                    = 'Search Ads'
+Search_ads_max_name_length                  = 130
+Search_ads_min_bid                          = '100'
+Search_ads_max_bid                          = '100,000'
+Search_ads_min_daily_limit_budget           = '14,000'
+Search_ads_max_daily_limit_budget           = '700,000,000'
+
+################# Search Banner info ####################
+
+Search_banner_location                      = 'Search Result'
+Search_banner_campaign_type                 = 'Search Banner'
+Search_banner_max_name_length               = 130
+Search_banner_min_daily_limit_budget        = '14,000'
+Search_banner_max_daily_limit_budget        = '700,000,000'
+
+################# New Tab Banner ####################
+
+New_tab_banner_location                     = 'New Tab'
+New_tab_banner_position                     = 'New Tab Central Banner'
+New_tab_banner_campaign_type                = 'New Tab Banner'
+New_tab_banner_max_name_length              = 130
+New_tab_banner_min_bid                      = '2,500'
+New_tab_banner_max_bid                      = '100,000'
+New_tab_banner_min_daily_limit_budget       = '285,000'
+New_tab_banner_max_daily_limit_budget       = '700,000,000'
+New_tab_banner_min_impressions              = '1'
+New_tab_banner_max_impressions              = '1,000,000,000'
+
+################# New Tab Banner ####################
+
+Icon_ads_location                           = 'New Tab'
+Icon_ads_position                           = 'Icon'
+Icon_ads_campaign_type                      = 'Icon Ads'
+Icon_ads_max_name_length                    = 130
+Icon_ads_min_daily_limit_budget             = '14,000'
+Icon_ads_max_daily_limit_budget             = '700,000,000'
+
+################# Small Native In-feed Banner ####################
+Small_zen_location                          = 'New Tab'
+Small_zen_position                          = 'Small In-feed Banner'
+Small_zen_campaign_type                     = 'Small Native In-feed Banner'
+Small_zen_max_name_length                   = 130
+Small_zen_min_daily_limit_budget            = '14,000'
+Small_zen_max_daily_limit_budget            = '700,000,000'
+
+################# Small Native In-feed Banner ####################
+Shopping_ads_location                       = 'Search Result'
+Shopping_ads_campaign_type                  = 'Shopping Ads'
+Shopping_ads_max_name_length                = 130
+Shopping_ads_min_daily_limit_budget         = '14,000'
+Shopping_ads_max_daily_limit_budget         = '700,000,000'
+
+
+################### NAPAS  ########################################
+Napas_min_amount_first_time                 = '1000000'
+Napas_max_amount                            = '500000000'
+Napas_min_amount_second_time                = '500000'
+
+################### BANK Transfer #################################
+Bank_min_amount_first_time                  = '1000000'
+Bank_min_amount_second_time                 = '500000'
+Bank_max_amount                             = '2000000000'
